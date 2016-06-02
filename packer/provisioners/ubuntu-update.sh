ccfcc038e593d7e865978f3e9aa683b93f241beb
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-06-02 14:03:03 +0200

    update once

--- packer/provisioners/ubuntu-update.sh
+++ packer/provisioners/ubuntu-update.sh
@@ -4,8 +4,9 @@
 set -e
 
 echo "Updating package info..."
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D #Docker
+sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list' #Docker
 sudo apt-get update -y
 
 echo "Upgrading packages..."
 sudo apt-get upgrade -y
-
