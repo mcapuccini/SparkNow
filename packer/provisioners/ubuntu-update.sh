47213921ce8d25c9105772c0155078893e4a1691
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-06-02 14:05:43 +0200

    fix hashsum mismatch

--- packer/provisioners/ubuntu-update.sh
+++ packer/provisioners/ubuntu-update.sh
@@ -6,6 +6,7 @@ set -e
 echo "Updating package info..."
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D #Docker
 sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list' #Docker
+sudo rm -rf /var/lib/apt/lists/*
 sudo apt-get update -y
 
 echo "Upgrading packages..."
