f0c7283a6e17ad8dd3d8670d0d80e52cd5babb38
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-06-02 13:33:10 +0200

    build jupyter with a given version

--- packer/provisioners/install-jupyter.sh
+++ packer/provisioners/install-jupyter.sh
@@ -3,5 +3,17 @@
 #Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "Pulling jupyter/all-spark-notebook docker image..."
-sudo docker pull jupyter/all-spark-notebook
+echo "Installing git..."
+sudo apt-get install -y git
+
+echo "Pulling jupyter/docker-stacks..."
+git clone https://github.com/jupyter/docker-stacks.git
+
+echo "Building pyspark-notebook..."
+sudo docker build docker-stacks/pyspark-notebook \
+  --build-arg APACHE_SPARK_VERSION=$SPARK_VERSION \
+  -t jupyter/pyspark-notebook
+
+echo "Building all-spark-notebook..."
+sudo docker build docker-stacks/all-spark-notebook \
+  -t jupyter/all-spark-notebook
