f1ac495a05b5f2f4eb95cb8a05d8200f04d19cab
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-06-03 16:13:53 +0200

    minor fix

--- packer/provisioners/install-consul.sh
+++ packer/provisioners/install-consul.sh
@@ -16,7 +16,7 @@ sudo unzip /tmp/$CONSUL_ZIP -d /usr/bin
 sudo mkdir /tmp/consul
 sudo mkdir /etc/consul.d
 sudo mkdir /var/log/consul
-sudo sh -c 'echo "{\"ports\": {\"dns\": 53}, \"recursor\": \"8.8.8.8\"}" > /tmp/consul.d/conf.json'
+sudo sh -c 'echo "{\"ports\": {\"dns\": 53}, \"recursor\": \"8.8.8.8\"}" > /etc/consul.d/conf.json'
 sudo sh -c "echo \"nameserver 127.0.0.1\" > /etc/resolv.conf"
 
 echo "$CONSUL_PACKAGE_NAME installation complete."
