20b86f2cf400a8962eae2c437f0bfef9ea77e53f
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-06-04 17:05:25 +0200

    fix HADOOP_OPTS

--- packer/provisioners/install-hdfs.sh
+++ packer/provisioners/install-hdfs.sh
@@ -34,7 +34,7 @@ echo 'export HADOOP_COMMON_HOME=$HADOOP_HOME' >> ~/.bashrc
 echo 'export HADOOP_HDFS_HOME=$HADOOP_HOME' >> ~/.bashrc
 echo 'export YARN_HOME=$HADOOP_HOME' >> ~/.bashrc
 echo 'export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native' >> ~/.bashrc
-echo 'export HADOOP_OPTS="Djava.library.path=$HADOOP_HOME/lib"' >> ~/.bashrc
+echo 'export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib"' >> ~/.bashrc
 sudo sh -c 'echo "JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /opt/hadoop/default/etc/hadoop/hadoop-env.sh'
 
 echo "$HADOOP_PACKAGE_NAME installation complete."
