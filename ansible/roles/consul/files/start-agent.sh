3f0613d570612c00df807ee933b2b2da77c45d46
Ubuntu <ubuntu@test.node.dc1.consul>
2016-06-08 14:38:34 +0000

    fixes

--- ansible/roles/consul/files/start-agent.sh
+++ ansible/roles/consul/files/start-agent.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Starting consul server agent
-sudo consul agent \
+sudo daemon -- consul agent \
   -data-dir /tmp/consul \
   -node=$(hostname) \
   -config-dir /etc/consul.d \
   -bind $(hostname -I | awk '{print $1;}') \
   -dc $CONSUL_DC_NAME \
-  > /var/log/consul/consul.log &
+  > /var/log/consul/consul.log
