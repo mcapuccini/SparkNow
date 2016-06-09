c74a3bb6711b52e176f709d2ccf5438d84d38b4c
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-06-09 16:26:53 +0200

    bind to hostname

--- ansible/roles/spark/files/start-master.sh
+++ ansible/roles/spark/files/start-master.sh
@@ -3,4 +3,4 @@
 #Starting Spark master
 export SPARK_PUBLIC_DNS=$(hostname)
 cd /opt/spark/default
-sbin/start-master.sh
+sbin/start-master.sh -h $(hostname)
--- ansible/roles/spark/files/start-slave.sh
+++ ansible/roles/spark/files/start-slave.sh
@@ -3,4 +3,4 @@
 #Starting Spark slave
 export SPARK_PUBLIC_DNS=$(hostname)
 cd /opt/spark/default
-sbin/start-slave.sh spark://$SPARK_MASTER_HOST:7077
+sbin/start-slave.sh -h $(hostname) spark://$SPARK_MASTER_HOST:7077
