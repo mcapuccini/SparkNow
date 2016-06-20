87534c3a89dbaf3d3807616df982df2daa290aa2
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2016-06-20 11:19:51 +0200

    set hadoop conf dir

--- packer/provisioners/install-spark.sh
+++ packer/provisioners/install-spark.sh
@@ -14,4 +14,7 @@ sudo tar xzf /tmp/$SPARK_TGZ -C /opt/spark/
 sudo ln -s /opt/spark/$SPARK_PACKAGE_NAME /opt/spark/default
 sudo chown -R ubuntu:ubuntu /opt/spark
 
+echo "Setting HADOOP_CONF_DIR..."
+echo "export HADOOP_CONF_DIR=/opt/hadoop/default/etc/hadoop" > /opt/spark/default/conf/spark-env.sh
+
 echo "$SPARK_PACKAGE_NAME installation complete."
