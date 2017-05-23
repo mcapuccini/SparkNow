51361795009a696a19c132e1e2206b4a1439b963
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2017-05-23 11:34:08 +0200

    run zeppelin in host network

--- ansible/roles/spark/files/start-master.sh
+++ ansible/roles/spark/files/start-master.sh
@@ -2,5 +2,4 @@
 
 #Starting Spark master
 cd /opt/spark/default
-sudo -u ubuntu SPARK_PUBLIC_DNS=$(hostname) sbin/start-master.sh \
-  -h $(hostname) $(hostname -I | awk '{print $1;}')
+sudo -u ubuntu SPARK_PUBLIC_DNS=$(hostname) sbin/start-master.sh -h $(hostname)
--- ansible/roles/spark/files/start-zeppelin.sh
+++ ansible/roles/spark/files/start-zeppelin.sh
@@ -5,8 +5,9 @@
 sudo docker run -d \
   -v /mnt/volume/zeppelin-workspace:/zeppelin-workspace \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -p 9999:8080 \
-  -e MASTER=spark://$(hostname -I | awk '{print $1;}'):7077 \
+  --net=host \
+  -e MASTER=spark://$(hostname):7077 \
+  -e ZEPPELIN_PORT=9999 \
   -e ZEPPELIN_NOTEBOOK_DIR=/zeppelin-workspace \
   -e SPARK_SUBMIT_OPTIONS="--conf spark.rpc=$SPARK_RPC" \
   dylanmei/zeppelin:0.7.1
