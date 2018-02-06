d683f7406959769555a97a9d6b43ddad31e2a3a4
Juan Inda <indajuan@gmail.com>
2018-02-06 14:19:20 +0100

    Java version, docker install, hadoop version, add OS_TENANT_NAME OS_TENANT_ID to v3 RC file

--- packer/provisioners/common.sh
+++ packer/provisioners/common.sh
@@ -6,8 +6,8 @@ set -e
 echo "Updating package info..."
 
 #Docker
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list'
+#sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+#sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list'
 
 sudo apt-get update -y
 
@@ -15,8 +15,8 @@ echo "Upgrading packages..."
 sudo apt-get upgrade -y
 
 echo "Installing java..."
-sudo apt-get install -y openjdk-7-jre-headless
-echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> ~/.bashrc
+sudo apt-get install -y openjdk-8-jre
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> ~/.bashrc
 
 echo "Installing python packages..."
 sudo apt-get install -y \
--- packer/provisioners/install-docker.sh
+++ packer/provisioners/install-docker.sh
@@ -6,8 +6,32 @@ set -e
 echo "Installing apparmor..."
 sudo apt-get install -y apparmor
 
+
 echo "Installing docker..."
-sudo apt-get install -y docker-engine
+#sudo apt-get install -y docker-engine
+
+
+sudo apt-get install --no-install-recommends \
+    apt-transport-https \
+    curl \
+    software-properties-common
+
+sudo apt-get install -y --no-install-recommends \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+
+curl -fsSL 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb https://packages.docker.com/1.13/apt/repo/ \
+   ubuntu-$(lsb_release -cs) \
+   main"
+
+sudo apt-get update
+
+sudo apt-get -y install docker-engine
+
 
 echo "Add ubuntu to docker group..."
 sudo gpasswd -a ubuntu docker
